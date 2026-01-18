08efdadcc3405845108edf9d8b337cd7ff720f49
Jury Giannelli <jury.giannelli@sdox.io>
2026-01-18 16:14:10 +0100

    add tracks container

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -1,3 +1,5 @@
+import { PropsWithChildren } from "react"
+
 type Track = {
   id: string
   active: boolean
@@ -7,24 +9,36 @@ interface TracksProps {
   tracks?: Track[]
 }
 
+const Container = ({children}: PropsWithChildren) => (
+  <div className="flex-1 overflow-y-auto pt-12 pb-32">
+    <div className="bg-[#101010] p-6 font-mono">
+      {children}
+    </div>
+  </div>
+)
+
 export const Tracks = ({tracks = []}: TracksProps) => {
   if (tracks.length === 0) {
     return (
-      <div className="text-neutral-300/30 text-[11px]">
-        No tracks to display.
-      </div>
+      <Container>
+        <div className="text-neutral-300/30 text-[11px]">
+          No tracks to display.
+        </div>
+      </Container>
     )
   }
 
   return (
-    <div className="flex flex-col gap-8 px-8">
-      {tracks
-        .filter((track) => track.active)
-        .map(({id}) => (
-          <div key={id}>
-            track id: {id}
-          </div>
-        ))}
-    </div>
+    <Container>
+      <div className="flex flex-col gap-8 px-8">
+        {tracks
+          .filter((track) => track.active)
+          .map(({id}) => (
+            <div key={id}>
+              track id: {id}
+            </div>
+          ))}
+      </div>
+    </Container>
   )
 }
