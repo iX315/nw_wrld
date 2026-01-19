3f70894d37bca546ae9c2602e023ecb9616ab6ad
Jury Giannelli <iX315@me.com>
2026-01-19 19:31:20 +0100

    transform src/index in ts

--- src/index.js
+++
@@ -1,3 +0,0 @@
-const path = require("path");
-
-require(path.join(__dirname, "..", "dist", "runtime", "main", "mainProcess", "entry.js")).start();
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import { createRequire } from "node:module"
+
+const require = createRequire(import.meta.url)
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+import { start } from "./main/mainProcess/entry"
+
+require('node:os')
+
+// The built directory structure
+//
+// ├─┬─┬ dist
+// │ │ └── index.html
+// │ │
+// │ ├─┬ dist-electron
+// │ │ ├── index.js
+// │ │ └── preload.mjs
+// │
+
+process.env.APP_ROOT = path.join(__dirname, '..')
+
+// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
+export const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron')
+export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
+
+process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST
+
+// start electron main process
+start()
