a00d6d261f519c8fa2ba54aac284c6d6e1df17b1
Jury Giannelli <iX315@me.com>
2026-01-19 19:28:30 +0100

    add vite config

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
+import { vitePrerenderPlugin } from 'vite-prerender-plugin'
+import electron from 'vite-plugin-electron/simple'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    vitePrerenderPlugin({
+      renderTarget: '#root',
+    }),
+    electron({
+      main: {
+        // Shortcut of `build.lib.entry`.
+        entry: 'src/index.ts'
+      },
+      preload: {
+        // Shortcut of `build.rollupOptions.input`.
+        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+        input: path.join(__dirname, 'src/preload.ts')
+      },
+      // Ployfill the Electron and Node.js API for Renderer process.
+      // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
+      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+      renderer: process.env.NODE_ENV === 'test'
+        // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
+        ? undefined
+        : {}
+    })
+  ]
+})
